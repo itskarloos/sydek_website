c7b03e0170aee0b2d772c4391c1ea1d018551d0a
Nahom Tegaude <nahomteguade@gmail.com>
2023-10-24 20:31:20 +0300

    About Page Hero Section

--- src/Components/AboutPage.jsx
+++ src/Components/AboutPage.jsx
@@ -6,18 +6,16 @@ const AboutPage = () => {
         <Fragment>
             <div className="AboutContainer">
                 <div className="About-Hero">
-                    <h1 className="Handle">
+                    <h1 className="Handle font-manrope font-semibold ss:text-[72px] text-[52px] text-white z-[1] ">
                         About Us
                     </h1>
-                    <p className="discription">
-                    That&lsquos why we&lsquore committed to delivering innovative IT solutions to 
-                    businesses of all sizes. Our team of experienced 
-                    professionals is dedicated to helping you achieve your 
-                    goals and thrive in a rapidly evolving digital landscape.
+                    <p className="discription font-manrope font-light text-[20px] text-white z-[1] ">
+                    Our development team is competitive because we are experts in the latest technologies, 
+                    have a proven track record of success, and are committed to excellence.
                     </p>
                 </div>
                 <div className="MainDescription">
-                    <h3 className="Moto">
+                    <h3 className="Moto font-manrope font-semibold text-[32px] text-white z-[1]">
                         We believe that technology can change the WORLD.
                     </h3>
                     <p className="explianation">
