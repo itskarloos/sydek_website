8c107a8264532f8b8eef8ef8d5366be8653f078c
Nahom Tegaude <nahomteguade@gmail.com>
2023-10-24 13:24:54 +0300

    Main App Clean Up

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // Import necessary styles and components
 import styles from "./style";
+import { useEffect, useRef } from "react";
+import { motion, useInView, useAnimation } from "framer-motion";
 import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {
   LandingPage,
@@ -10,7 +12,28 @@ import {
 } from "./Components";
 
 const App = () => {
+  const ref0 = useRef(null);
+  const isInView0 = useInView({ ref: ref0, once: true });
+  const mainControls0 = useAnimation();
 
+  // Effect to trigger animation when Hero section is in view
+  useEffect(() => {
+    if (isInView0) {
+      mainControls0.start("visible");
+    }
+  }, [isInView0]);
+
+  // Create a ref and useInView hook for the Service section
+  const ref1 = useRef(null);
+  const isInView1 = useInView({ ref: ref1, once: true });
+  const mainControls1 = useAnimation();
+
+  // Effect to trigger animation when Service section is in view
+  useEffect(() => {
+    if (isInView1) {
+      mainControls1.start("visible");
+    }
+  }, [isInView1]);
 
   return (
     <div className="bg-primary w-full overflow-hidden">
