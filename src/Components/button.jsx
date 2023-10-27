451b4f01979bf9d856f55f20e7034de6b33032c1
Nahom Tegaude <nahomteguade@gmail.com>
2023-10-27 04:53:34 +0300

    Hero Section Button Linked

--- src/Components/button.jsx
+++ src/Components/button.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 import styles from '../style'
 const button = () => (
     <div className={`${styles.flexCenter} w-[130px] h-[50px] rounded-[5px] bg-white p-[2px] cursor-pointer` }>
       <div className={`${styles.flexCenter} flex-col bg-white h-[100%] rounded-[5px] justify-center`}>
         <p className='font-poppins font-thin text-[15px] leading-[23px] text-black'>
-          Our Service
+          <a href="./Service">Our Service</a>
         </p>
         </div>
     </div>
--- src/Components/button2.jsx
+++ src/Components/button2.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import styles from '../style'
+import React from 'react';
+import styles from '../style';
 const button2 = () => (
     <div className={`${styles.flexCenter} w-[130px] h-[50px] rounded-[5px] bg-white p-[1px] cursor-pointer` }>
       <div className={`${styles.flexCenter} flex-col bg-black h-[100%] w-[100%] rounded-[5px] justify-center`}>
         <p className='font-poppins font-thin text-[15px] leading-[23px] text-white'>
-          Contact us
+        <a href="./About">Contact Us</a>
         </p>
         </div>
     </div>
