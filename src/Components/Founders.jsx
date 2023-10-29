ad441e36094fc8efceae89aa2b539af74299fa82
Michael <mikenegh@gmail.com>
2023-10-29 17:02:43 +0300

    Founders page Div Fixed

--- src/Components/Founders.jsx
+++ src/Components/Founders.jsx
@@ -6,16 +6,26 @@ import { Fragment } from "react";
 const Founders = () => {
   return (
     <Fragment>
-      <section id="home" className="flex-col">
-        <div className={`flex-1 ${styles.flexStart} flex-col`}>
+      <section className={` flex-col ${styles.paddingY}`}>
+        <div
+          className={`flex-1 ${styles.flexStart} flex-col xl:px-16 sm:px-16 px-6`}
+        >
           <div className="flex flex-row justify-between w-[100%] flex-wrap">
             <div className="service-text">
-              <h1 className="flex-1 font-manrope font-semibold ss:text-[72px] text-[52px] text-gradient ss:leading-[100px] leading-[60px] z-[1] ">
+              <h1 className="flex-1 font-manrope font-semibold ss:text-[60px] text-[45px] text-gradient text-white ss:leading-[100px] leading-[60px] z-[1] ">
                 Founders
               </h1>
             </div>
+
+            <p className={`${styles.paragraph} font-thin`}>
+              Our Teams are a group of highly talented and experienced
+              individuals who are passionate about our company's mission. They
+              have a proven track record of success in the industry, and they
+              are committed to making a difference in the world
+            </p>
           </div>
         </div>
+
         <div className="flex flex-row justify-around flex-wrap mt-[20px]">
           <div className="founder-container">
             {founderInfo.map((info) => (
