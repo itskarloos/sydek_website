62adfaf330d5b1a4ff96f1539daab7e3b34b918f
Nahom Tegaude <nahomteguade@gmail.com>
2023-11-04 11:28:29 +0300

    Hero Section Buttons Fixed

--- src/Components/button.jsx
+++ src/Components/button.jsx
@@ -1,11 +1,9 @@
 import styles from '../style';
 import { Link } from "react-router-dom";
 import ErrorBoundary from './ErrorBoundary';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 const button = () => (
     <ErrorBoundary>
-        <Router>
           <div className={`${styles.flexCenter} w-[130px] h-[50px] rounded-[5px] bg-white p-[2px] cursor-pointer` }>
             <div className={`${styles.flexCenter} flex-col bg-white h-[100%] rounded-[5px] justify-center`}>
               <p className='font-poppins font-thin text-[15px] leading-[23px] text-black'>
@@ -13,7 +11,6 @@ const button = () => (
               </p>
               </div>
           </div>
-        </Router>
     </ErrorBoundary>
 )
 export default button;
--- src/Components/button2.jsx
+++ src/Components/button2.jsx
@@ -1,11 +1,9 @@
 import styles from '../style';
 import { Link } from "react-router-dom";
 import ErrorBoundary from './ErrorBoundary';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 const button2 = () => (
     <ErrorBoundary>
-      <Router>
         <div className={`${styles.flexCenter} w-[130px] h-[50px] rounded-[5px] bg-white p-[1px] cursor-pointer` }>
         <div className={`${styles.flexCenter} flex-col bg-black h-[100%] w-[100%] rounded-[5px] justify-center`}>
           <p className='font-poppins font-thin text-[15px] leading-[23px] text-white'>
@@ -13,7 +11,6 @@ const button2 = () => (
           </p>
           </div>
         </div>
-      </Router>
     </ErrorBoundary>
 )
 export default button2
